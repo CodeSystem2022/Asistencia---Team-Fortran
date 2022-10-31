255bfae7e28b78825af0c70ddcaa708e5af87f41
Fernando <califerdan@gmail.com>
2022-10-31 11:38:59 -0300

    asistencia fernando calisaya

--- asistencia.py
+++ asistencia.py
@@ -28,3 +28,24 @@ persona1 = Persona('Ariel', 'Betancud', 355855,40) # Nesecitamos enviar argument
 print(f'El objeto1 de la clase persona: {persona1.nombre} {persona1.apellido} Su edad es: {persona1.edad}')
 
 """
+
+# Fernando Calisaya
+class Aritmetica:
+    def __init__(self, operandoA, operandoB):
+        self.operandoA = operandoA
+        self.operandoB = operandoB
+    def sumar(self):
+        return self.operandoA + self.operandoB
+    def resta(self):
+        return self.operandoA - self.operandoB
+    def multiplicar(self):
+        return self.operandoA * self.operandoB
+    def dividir(self):
+        return self.operandoA / self.operandoB
+
+aritmetica1 = Aritmetica(49, 7)
+print(f'La suma de los números es: {aritmetica1.sumar()}')
+print(f'la resta de los números es: {aritmetica1.resta()}')
+print(f'La multiplicación de los números es: {aritmetica1.multiplicar()}')
+print(f'La división de los números es: {aritmetica1.dividir():.2f}')
+# ------------------------------------
