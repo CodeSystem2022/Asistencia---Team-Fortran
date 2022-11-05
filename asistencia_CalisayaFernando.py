f410a2857099c7308b40013280490c6455430aad
Fernando <califerdan@gmail.com>
2022-11-05 17:41:10 -0300

    Asistencia Calisaya Fernando

---
+++ asistencia_CalisayaFernando.py
@@ -0,0 +1,18 @@
+class Aritmetica:
+    def __init__(self, operandoA, operandoB):
+        self.operandoA = operandoA
+        self.operandoB = operandoB
+    def sumar(self):
+        return self.operandoA + self.operandoB
+    def resta(self):
+        return self.operandoA - self.operandoB
+    def multiplicar(self):
+        return self.operandoA * self.operandoB
+    def dividir(self):
+        return self.operandoA / self.operandoB
+
+aritmetica1 = Aritmetica(49, 7)
+print(f'La suma de los números es: {aritmetica1.sumar()}')
+print(f'la resta de los números es: {aritmetica1.resta()}')
+print(f'La multiplicación de los números es: {aritmetica1.multiplicar()}')
+print(f'La división de los números es: {aritmetica1.dividir():.2f}')
