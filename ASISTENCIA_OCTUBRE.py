7f7cb4e8a2125ef0a626285e5b660623a28d2e67
danisa-morales <64617071+danisa-morales@users.noreply.github.com>
2022-11-05 17:36:21 -0300

    Asistencia Octubre - Morales Gaston

--- ASISTENCIA_OCTUBRE.py
+++ ASISTENCIA_OCTUBRE.py
@@ -246,5 +246,31 @@ if __name__ == "__main__":
 
     print(__name__)
     
-    #---------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------
+    
+    
+    
+# MORALES GASTON
+    
+    class Rectangulo:
+    """
+    Crear una class llamada Rectangulo, debe tener  2 atributas: altura y base
+    el nombre del metodo sera calcular area utilizando la formula:
+    area = base * altura. Pero la base y la altura deben ser ingresadas
+    por el usuario y los objetos deben ser tres.
+    """
+    def __init__(self, altura, base):
+        self.altura = altura
+        self.base = base
+
+    def calcular_area(self):
+        return self.base * self.altura
+
+base = init(input("Digite el numero para la base del rectangulo: "))
+altura = init(input("Digite el numero para la altura del rectangulo: "))
+rectangulo1 = rectangulo(base, altura)
+print(f"El area del rectangulo es: {rectangulo1.calcular_area()}")
+
+#--------------------------------------------------------------------------------------------------------
+
 
