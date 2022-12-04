80eb04da1ff65accd5f00b0c7c944038762fe199
Fernando <califerdan@gmail.com>
2022-12-04 17:37:11 -0300

    Asistencia Calisaya Fernando

--- ASISTENCIA_NOVIEMBRE.py
+++ ASISTENCIA_NOVIEMBRE.py
@@ -85,3 +85,19 @@ miObjeto1.metodo_clase()
 miObjeto1.metodo_instancia()
 
 #--------------------------------------------------------------------------------------------------------------
+
+# CALISAYA FERNANDO DANIEL
+
+class Empleado:  # No hereda sino solo de la clase object
+    def __init__(self, nombre, sueldo):
+        self.nombre = nombre
+        self.sueldo = sueldo
+
+    def __str__(self):
+        return f'Empleado: [Nombre: {self.nombre}, Sueldo: {self.sueldo}]'
+
+    def mostrar_detalles(self):
+        return self.__str__()
+
+
+#--------------------------------------------------------------------------------------------------------------
