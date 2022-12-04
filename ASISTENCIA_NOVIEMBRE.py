f0e2488a70fd21582177c6134b1630cd5abf905a
Facundo Pereyra <103546425+facundoPereyra07@users.noreply.github.com>
2022-12-04 14:58:36 -0300

    Update ASISTENCIA_NOVIEMBRE.py

--- ASISTENCIA_NOVIEMBRE.py
+++ ASISTENCIA_NOVIEMBRE.py
@@ -37,3 +37,51 @@ print(persona4)
 print(f'Valor contador personas: {Persona.contador_personas}')
 
 #--------------------------------------------------------------------------------------------------------------
+
+
+#FACUNDO PEREYRA
+
+
+class MiClase:
+    # Variable de clase, este atributo dara a cada objeto el mismo valor
+    variable_clase = 'Esta es una variable de clase'
+
+    def __init__(self, variable_instancia): #Variable de instancia
+        self.variable_instancia = variable_instancia
+
+    @staticmethod
+    def metodo_estatico():
+        print(MiClase.variable_clase)
+
+    @classmethod
+    def metodo_clase(cls):
+        print(cls.variable_clase)
+def metodo_instancia(self):
+        self.metodo_clase()
+        self.metodo_estatico()
+        print(self.variable_clase)
+        print(self.variable_instancia)
+
+print(MiClase.variable_clase)
+
+miClase1 = MiClase('Esta es una variable de instancia')
+print(miClase1.variable_instancia)
+print(miClase1.variable_clase)
+miClase2 = MiClase("Esta es otra prueba de variable de instancia")
+print(miClase2.variable_instancia)
+print(miClase2.variable_clase)
+MiClase.variable_clase2 = 'Valor de variable de clase 2'
+print(MiClase.variable_clase2)
+print(miClase1.variable_clase2)
+print(miClase2.variable_clase2)
+
+MiClase.metodo_estatico()
+
+MiClase.metodo_clase()
+
+
+miObjeto1 = MiClase('variable de instancia')
+miObjeto1.metodo_clase()
+miObjeto1.metodo_instancia()
+
+#--------------------------------------------------------------------------------------------------------------
